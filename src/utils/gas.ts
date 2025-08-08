1d6278b87d98b6e5c537ebab2097a098b08fc688
Aime Comeault <robinhoodblock71@gmail.com>
2025-08-08 13:02:52 -0600

    feat: add gas cost estimation utility

---
+++ src/utils/gas.ts
@@ -0,0 +1,28 @@
+import { type Provider } from 'ethers';
+
+export interface EstimateGasUsdParams {
+  /** Provider for accessing network fee data */
+  provider: Provider;
+  /** Number of gas units the transaction is expected to consume */
+  gasUnits: bigint;
+  /** Current ETH price in USD for normalization */
+  ethUsd: number;
+}
+
+/**
+ * Estimates the USD cost of executing a transaction by fetching
+ * current gas fee information from the provider and converting the
+ * expected gas usage into a USD value.
+ */
+export async function estimateGasUsd(
+  { provider, gasUnits, ethUsd }: EstimateGasUsdParams
+): Promise<number> {
+  const { maxFeePerGas, gasPrice } = await provider.getFeeData();
+  const priceWei = maxFeePerGas ?? gasPrice;
+  if (!priceWei) {
+    throw new Error('Gas price data not available');
+  }
+  const wei = priceWei * gasUnits;
+  const eth = Number(wei) / 1e18;
+  return eth * ethUsd;
+}
